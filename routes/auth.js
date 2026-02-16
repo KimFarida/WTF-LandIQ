cccf4caf2329dd5dcb11daa4a7f4143500d9bf92
Kim Farida <farimomoh@gmail.com>
2026-02-16 10:36:07 +0100

    Feat: Implemented Login Endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
-const { registerRules } = require('../middleware/authValidator');
+const { registerRules, loginRules } = require('../middleware/authValidator');
 
 router.get('/', (req, res)=>{
     res.send("Welcome to Auth Route")
 })
 
 router.post('/register',registerRules, authController.registerUser)
+router.post('/login', loginRules, authController.loginUser)
 
 module.exports = router;
