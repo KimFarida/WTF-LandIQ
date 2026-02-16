6bc638cbf68de3cbdc6663a36b1081bdc4039587
Kim Farida <farimomoh@gmail.com>
2026-02-16 14:28:24 +0100

    Fix: Changed userID to UUID

--- models/user.js
+++ models/user.js
@@ -16,9 +16,9 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: DataTypes.INTEGER
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
       },
       firstName: {
         allowNull: false,
