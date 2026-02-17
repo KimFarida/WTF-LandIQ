d7067450f136938dc14a995bc17460ffb7404177
Kim Farida <farimomoh@gmail.com>
2026-02-17 11:16:19 +0100

    Feat: Included Assessment Routes | GeoLookup Startup

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+
+const db = require('./models/index');
+
 const authRoutes = require('./routes/auth');
+const assessmentRoutes = require('./routes/assessment');
+
+const geoLookupService = require('./services/geoLookupService');
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(cookieParser());
 
-
-const db = require('./models/index');
+// db startup
 db.sequelize
 .sync()
 .then(()=>{
@@ -18,12 +24,22 @@ db.sequelize
 console.log("Failed to sync db:  " +  err.message);
 });
 
+// geolookup service startup
+try {
+  geoLookupService.loadGeoJSON();
+  console.log('✅ Geospatial lookup service ready');
+} catch (error) {
+  console.error('❌ Failed to load GeoJSON:', error.message);
+  process.exit(1); // Exit if GeoJSON can't be loaded
+}
 
+
+// Routes
 app.get('/', (req, res)=>{
     res.send("HELLO WORLD")
 })
-
 app.use('/api/auth', authRoutes);
+app.use('/api/assessments', assessmentRoutes);
 
 app.listen(5000, ()=>{
     console.log("App started")
